2b2f66172e79befcf237a5678240086670bb7f24
Judah Abraham <judahabrahamforgood@gmail.com>
2025-07-30 10:16:07 +0100

    first commit

---
+++ src/Frontend/Components/Navbar.tsx
@@ -0,0 +1,14 @@
+//! import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav
+      className="fixed top-0 left-0 flex items-center justify-between
+     bg-[#1e293b] w-[100%] p-[2rem]  h-[80px] mt-0 mr-auto ml-auto "
+    >
+      <h1 className="text-3xl text-white font-semibold font-nunito">Jshare</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
