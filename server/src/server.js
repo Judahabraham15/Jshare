f8853ba6698164048b39608dc872a730041170b4
Judah Abraham <judahabrahamforgood@gmail.com>
2025-08-08 17:31:53 +0100

    Made progress with the backend. Fixed the directory folder bug.

--- server/src/server.js
+++ server/src/server.js
@@ -2,15 +2,20 @@ const express = require("express");
 const multer = require("multer");
 const cors = require("cors");
 const path = require("path");
+const fs = require("fs");
 
+
+const uploadDir = "UPLOADS";
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 const app = express();
 //* Middleware
 app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 
 //* Serve uploaded files
-app.use("/files", express.static("uploads"));
-
+app.use("/files", express.static(path.join(__dirname, "UPLOADS")));
 
 // TODO: THERE ARE BUGS TO FIX!!!😭
 
@@ -18,7 +23,7 @@ app.use("/files", express.static("uploads"));
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
     //! The destination on where the files should go(i.e the folder it should go to.)
-    return callback(null, "UPLOADS/");
+     callback(null, `./UPLOADS`);
   },
   filename: (req, file, cb) => {
     const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9); //? Helped by Ai for this process
@@ -33,13 +38,35 @@ const Uploads = multer({
   limits: { fileSize: 100 * 1024 * 1024 }, //? 100MB limit bro
 });
 
-//* UPLOAD ROUTE
-app.post("/upload", Uploads.single("file"), (res, req) => {
-  console.log(req.body);
-  console.log(req.file);
+//* UPLOAD ROUTE FOR SINGLE FILE
+app.post("/upload", Uploads.single("file"), (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ error: "No Files Uploaded" });
+    }
+    const fileLink = {
+      file: req.file.originalname,
+      link: `http://localhost:5174/files/${req.file.filename}`,
+    };
+    res.status(200).json(fileLink)
+  } catch (error) {
+    console.error("Upload error:", error);
+    if (error instanceof multer.MulterError) {
+      if (error.code === "LIMIT_FILE_SIZE") {
+        return res.status(413).json({ error: "File too large" });
+      }
+      return res.status(400).json({ error: error.message });
+    }
+    res.status(500).json({error: error.message || 'Upload Failed'})
+  }
 });
-// const PORT = 3001
-app.listen(3001, () => {
+
+//Test Route
+app.get('/' , (req, res)=>{
+  res.send('Server is running')
+})
+const PORT = 5174;
+app.listen(PORT, () => {
   console.log("Server is running!");
 });
 console.log("Hello");
